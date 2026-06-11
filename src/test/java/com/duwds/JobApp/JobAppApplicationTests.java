ff6769092d241eb0d94a861f7b9a68def741ff2f
Nguyen Trung Duc <duwdsnguyen@gmail.com>
2026-06-11 21:41:57 +0700

    project created

---
+++ src/test/java/com/duwds/JobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duwds.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
